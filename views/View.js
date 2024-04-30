58666a94d4969e30145764d957d0a2364a07378e
Nicole Parker <nicole@rocketdaddy.ai>
2024-04-30 16:27:25 -0400

    card design

---
+++ views/View.js
@@ -0,0 +1,16 @@
+export default class View {
+    _data;
+
+    // Render view
+
+    // Update view
+
+    // Clear view
+
+    // Render spinner
+
+    // Render error
+
+    // Render messages
+
+}
---
+++ views/clientsView.js
@@ -0,0 +1,18 @@
+class ClientView {
+    _generateMarkup() {
+        return `
+        <div class="card card--client">
+            <div class="card__img client-logo"></div>
+            <div class="card__content">
+                <h1 class="card__title client-name">${clientName}</h1>
+                <ul class="card__items">
+                    <li class="card__item">
+                        <div class="item-icon"></div>
+                        <span class="item-count location-count">${locationCount}</span> Locations
+                    </li>
+                </ul>
+            </div>
+        </div>
+        `
+    }
+}
