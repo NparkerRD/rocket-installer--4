ed1c30731184880115e18423238bbdf494355ca9
Nicole Parker <nicole@rocketdaddy.ai>
2024-04-30 13:21:40 -0400

    mobile navigation

---
+++ public/dist/js/nav.js
@@ -0,0 +1,27 @@
+const navBtn = document.querySelector(".nav-btn");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+let displayMenu = false;
+
+// Handlle overlay operations
+const openOverlay = () => overlay.classList.add("open");
+const closeOverlay = () => overlay.classList.remove("open");
+
+navBtn.addEventListener("click", () => {
+    if (!displayMenu) {
+        // Open the navigation menu
+        console.log(displayMenu);
+        openOverlay();
+        nav.classList.add("open");
+        burger.classList.add("open");
+        displayMenu = true;
+    } else if (displayMenu) {
+        // Close the navigation menu
+        console.log(displayMenu);
+        closeOverlay();
+        nav.classList.remove("open");
+        burger.classList.remove("open");
+        displayMenu = false;
+    }
+})
